ecd09477afc6ceb0c390c889ca352a8826d1e39d
isawzz <iwgajam@gmail.com>
2023-06-13 07:37:48 +0200

    das

--- pico0/source.js
+++ pico0/source.js
@@ -1,16 +1,19 @@
 function a(key, x) {
-  let ch = i[key]
-  let family = (ch[0] == 'f' || ch[0] == 'F') ? 'pictoFa' : 'pictoGame';
-	if (a == b){
-		console.log('das ist ein laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanger string!!!!!!!!!')}
-  let text = f('0x' + ch); //hallo
+  let ch = i[key];
+  let family = ch[0] == "f" || ch[0] == "F" ? "pictoFa" : "pictoGame";
+  if (a == b) {
+    console.log(
+      "das ist ein laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanger string!!!!!!!!!"
+    );
+  }
+  let text = f("0x" + ch); //hallo
 }
 // async function start(){return 2;}
 // // async function __start() {
 // //   onpagedeactivated(() => { fiddleSave(); dbSave(); }); //hallo
 // // }
 // // function _addFilterHighlight(mobj) { mobj.highC('green'); }
-// async function load_config_new() {      Config 
+// async function load_config_new() {      Config
 // 	= await route_path_yaml_dict('../y/config.yaml');
 //   let data          = await route_path_yaml_dict(
 // 		'../y/appdata.yaml');  for (const k in data) //das ist echt lustig!!!!!!!!!!
@@ -25,4 +28,3 @@ function a(key, x) {
 // C:\D\a04\py\twtbeginner
 //
 //
-
