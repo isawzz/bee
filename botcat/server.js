ccfbf762446c3b91054cc8278444785c5448594e
isawzz <iwgajam@gmail.com>
2023-06-13 02:44:11 +0200

    das

---
+++ botcat/server.js
@@ -0,0 +1,61 @@
+const discord = require("discord.js")
+//const fetch = require("node-fetch")
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const client = new discord.Client()
+const sadWords = ['sad','depressed','anxious','stress']
+const encouragements = [
+  'hang in there!',
+  'I look forward to see you soon!',
+  'oh, I feel for you!'
+]
+
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      return data[0]["q"] + " -" + data[0]["a"]
+    })
+}
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  console.log(`content ${msg.content} author:${msg.author.bot}`)
+  if (msg.content === "?") {
+    //msg.reply("yes, still here!")
+    getQuote().then(quote=>msg.channel.send(quote))
+  }
+
+  if (sadWords.some(x=>msg.content.includes(x))){
+    const enc = encouragements[Math.floor(Math.random()*encouragements.length)]
+    msg.reply(enc);
+  }
+})
+
+var TOKEN1 = process.env.TOKEN1
+client.login(TOKEN1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nodetest/string.js
@@ -0,0 +1,26 @@
+var s1='hallo';
+s1[1]='e'; // nein geht garnicht!!! efficiency reasons!
+//console.log(s1, s1[1])
+
+x='hallo das ist\r\n\f\tkomisch'.split(/\s+/)
+x='hallo das ist\r\n\f\tkomisch!?das;wirklich?'.split(/\s+/)
+//console.log(x)
+
+x=' hallo\n\t   '.trim();
+x='www.python.org'.trim('.worg')
+console.log(`|${x}|`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
